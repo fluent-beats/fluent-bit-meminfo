659f319c7df41737e426a91ff75454a25342e88e
cadu.goncalves <cadu.goncalves@gmail.com>
2023-01-28 21:59:07 -0300

    Adjusted license comments

--- code/in_meminfo/meminfo.h
+++ code/in_meminfo/meminfo.h
@@ -1,9 +1,7 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
-/*  Fluent Bit
+/*  Etriphany
  *  ==========
- *  Copyright (C) 2019-2021 The Fluent Bit Authors
- *  Copyright (C) 2015-2018 Treasure Data Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
